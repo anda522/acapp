ff2197d84d7258bc453132d3bf6937afc4c99a4d
anda522 <wyq522208@qq.com>
2022-05-08 23:42:19 +0800

    menu and playground

--- game/static/js/src/zbase.js
+++ game/static/js/src/zbase.js
@@ -1,5 +1,12 @@
 export class AcGame {
    constructor(id) {
-   console.log("This is a test!");
+       console.log("This is a test!");
+       this.id = id;
+       this.$ac_game = $('#' + id);
+       this.menu = new AcGameMenu(this);
+       this.playground = new AcGamePlayground(this);
+       this.start();
    }
+    start(){
+    }
 }
